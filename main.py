5f18b676ba1f83d44af5685334a7111e52dc548c
Rimpei2000 <70897624+Rimpei2000@users.noreply.github.com>
2022-11-07 20:25:55 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
+from bs4 import BeautifulSoup
+import requests
 
 file = open('info.json', 'r')
 info = json.load(file)
@@ -8,9 +10,14 @@ info = json.load(file)
 CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
+res = requests.get('https://www.rakuten-sec.co.jp/smartphone/market/info/pagecontent?pid=4021&sym=CADJPY%3DX')
+soup = BeautifulSoup(res.text, 'html.parser')
+price = soup.find("span", {"class": "base"}).text[0:6]
+
 def main():
     USER_ID = info['USER_ID']
-    message = TextSendMessage(text='Hello')
+    priceMsg = "$1 = " + price + "円"
+    message = TextSendMessage(text=priceMsg)
     line_bot_api.push_message(USER_ID, messages=message)
 
 if __name__ == "__main__":
